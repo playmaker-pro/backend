2599d80ce781b3fd84ce5457dac5d54abc87ee46
arsen <arsen@MBP-arsen.mshome.net>
2020-12-05 01:03:49 +0100

    fixig linkify errors with Manager account

--- profiles/views.py
+++ profiles/views.py
@@ -294,7 +294,7 @@ class ShowProfile(generic.TemplateView, mixins.ViewModalLoadingMixin):
             profile_model = get_profile_model_from_slug(slug)
             profile = get_object_or_404(profile_model, slug=slug)
             profile.history.increment()  # @todo 1 coomit to
-            if not self.request.user.is_anonymous::
+            if not self.request.user.is_anonymous:
                 if self.request.user.is_coach:
                     profile.history.increment_coach()
 
