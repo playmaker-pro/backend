df5ea1eae5360a613db683b88039e6bc5a27ccaf
arsen <arsen@MBP-arsen.mshome.net>
2020-12-07 19:20:06 +0100

    req bumpup

--- notifications/mass.py
+++ notifications/mass.py
@@ -1,5 +1,5 @@
 from django.core import mail
-    
+
 def send_mass():
     connection = mail.get_connection()
 
@@ -24,7 +24,16 @@ def send_mass():
 
 from django.core.mail import send_mass_mail
 
+
 def send_mass_basic():
-    message1 = ('Subject here', 'Here is the message', 'from@example.com', ['first@example.com', 'other@example.com'])
-    message2 = ('Another Subject', 'Here is another message', 'from@example.com', ['second@test.com'])
-    send_mass_mail((message1, message2), fail_silently=False)
+    msgs = []
+
+    for i in ['jacek.jasinski8@gmail.com', 'jjasinski.playmaker@gmail.com', 'rafal.kesik@gmail.com']:
+        
+        message = ('PlayMaker.pro nowa platforma',
+                   'Witaj, \n dziękujemy za dołączenie do społęczności PlayMaker.pro\n Przenieśliśmy Twoje konto na ziepłnie nową platformę. Wejdź i sprawdź nowe możliwości klikając w link. https://playmaker.pro/password/reset/?email={i} \n\n\n Pozdrawiamy zespół PlayMaker.pro',
+                   
+                   'powiadomienia@playmaker.pro', [i])
+        msgs.append(message)
+
+    send_mass_mail(msgs, fail_silently=False)
