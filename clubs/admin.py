c6b524f01b04f90292f8329083126eaf628ae524
arsen <arsen@MBP-arsen.mshome.net>
2020-12-11 14:20:46 +0100

    fixes for fb loging

--- clubs/admin.py
+++ clubs/admin.py
@@ -7,31 +7,31 @@ from . import models
 
 @admin.register(models.Seniority)
 class SeniorityAdmin(admin.ModelAdmin):
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 @admin.register(models.Gender)
 class GenderAdmin(admin.ModelAdmin):
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 @admin.register(models.League)
 class LeagueAdmin(admin.ModelAdmin):
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 @admin.register(models.Voivodeship)
 class VoivodeshipAdmin(admin.ModelAdmin):
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 @admin.register(models.Team)
 class TeamAdmin(admin.ModelAdmin):
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 @admin.register(models.Club)
 class ClubAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
 
-    search_fields = ('name')
+    search_fields = ('name',)
