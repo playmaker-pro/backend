f06b853b8b221fad1a63a1c33030bddb60be0fb3
rkesik <rafal.kesik@gmail.com>
2021-02-01 13:38:47 +0100

    weekly report missing cahr

--- bin/weekly.sh
+++ bin/weekly.sh
@@ -1,1 +1,1 @@
-source ~/venv/pro/bin/acticate && cd ~/prod/ && python manage.py send weekly --test rafal.kesik@gmail.com
+source ~/venv/pro/bin/activate && cd ~/prod/ && python manage.py send weekly --test rafal.kesik@gmail.com
