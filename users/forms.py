1e3ab00704a7238206249e2d0c4c72e7a7c8b01c
rkesik <rafal.kesik@gmail.com>
2021-04-11 21:59:53 +0200

    sadasd

--- users/forms.py
+++ users/forms.py
@@ -25,31 +25,36 @@ class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=20, label='First Name')
     last_name = forms.CharField(max_length=20, label='Last Name')
     role = forms.ChoiceField(choices=User.ROLE_CHOICES)
+    username = forms.CharField(max_length=255, label='username')
     # rodo = forms.BooleanField(required=True)
     # rules = forms.BooleanField(required=True)
     # club = forms.CharField(max_length=355, label='Club')
 
     def save(self, request):
-        adapter = get_adapter(request)
-        user = adapter.new_user(request)
+        username = request.POST.get('username')
+        #raise RuntimeError(f'{username}, {request.POST}')
+        if username == '':
+            adapter = get_adapter(request)
+            user = adapter.new_user(request)
 
-        # Here is access to pre-saved user model role - just to set proper Profile for user declared_type
-        user.declared_role = self.cleaned_data['role']
+            # Here is access to pre-saved user model role - just to set proper Profile for user declared_type
+            user.declared_role = self.cleaned_data['role']
 
-        # Below paramters will be visible in post_save under User signal to assign Profile
-        adapter.save_user(request, user, self)  # here kicks first object save()
+            # Below paramters will be visible in post_save under User signal to assign Profile
+            adapter.save_user(request, user, self)  # here kicks first object save()
 
-        self.custom_signup(request, user)
-        # TODO: Move into adapter `save_user` ?
-        setup_user_email(request, user, [])  # this will trigger pre-mature Fk relation assign (if User model is not saved before)
+            self.custom_signup(request, user)
+            # TODO: Move into adapter `save_user` ?
+            setup_user_email(request, user, [])  # this will trigger pre-mature Fk relation assign (if User model is not saved before)
 
-        #if self.cleaned_data['club']:
-        #    user.declared_club = self.cleaned_data['club']
-        #user.username = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
-        return user
+            #    user.declared_club = self.cleaned_data['club']
+            # user.username = self.cleaned_data['email']
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.save()
+            return user
+        else:
+            return None
 
     def save2222(self, request):
         user = super(CustomSignupForm, self).save(request)
