72cf446cdde562897d9095c07838900b2e14733b
rkesik <rafal.kesik@gmail.com>
2021-10-18 21:32:34 +0200

    optimalse next approach

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -483,7 +483,7 @@ def get_logging_structure(LOGFILE_ROOT):
             'django': {
                 'handlers': ['django_log_file'],
                 'propagate': True,
-                'level': 'DEBUG',
+                'level': 'ERROR',
             },
             'project': {
                 'handlers': ['proj_log_file'],
