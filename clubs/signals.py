79e1f284e1b5cc7f30e1889fc417682901827347
rkesik <rafal.kesik@gmail.com>
2022-02-11 11:58:30 +0100

    movging setting highest parent to singals module

---
+++ clubs/signals.py
@@ -0,0 +1,16 @@
+import logging
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from . import models
+
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=models.League)
+def _post_save_league(sender, instance, created, **kwargs):
+    # Identify and set highest parent
+    if highest_parent := instance.get_highest_parent():
+        instance.highest_parent = highest_parent
