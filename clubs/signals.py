8d8a8039ba3b00688655337c1b785f8bc24955cc
rkesik <rafal.kesik@gmail.com>
2022-02-11 12:04:53 +0100

    singal save for instance added

--- clubs/signals.py
+++ clubs/signals.py
@@ -12,5 +12,15 @@ logger = logging.getLogger(__name__)
 @receiver(post_save, sender=models.League)
 def _post_save_league(sender, instance, created, **kwargs):
     # Identify and set highest parent
+
+    if hasattr(instance, '_skip_post_save'):
+        return
+
     if highest_parent := instance.get_highest_parent():
         instance.highest_parent = highest_parent
+
+    try:
+        instance._skip_post_save = True
+        instance.save()
+    finally:
+        del instance._skip_post_save
