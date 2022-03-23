312ed2e3677b6135609422af6311e833b4f79117
Lukasz <l.remkowicz@gmail.com>
2022-03-23 11:02:33 +0100

    Fixed wrong league filter in soccerbase. Added icontans parameter

--- soccerbase/views.py
+++ soccerbase/views.py
@@ -70,9 +70,13 @@ class PlayersTable(TableView):
                 playerprofile__prefered_leg=self.filter_leg)
 
         if self.filter_league is not None:
-            queryset = queryset.filter(
-                playerprofile__team_object__league__highest_parent__name__in=self.filter_league
-                )
+            leagues = [league for league in self.filter_league]
+            league = (Q(
+                playerprofile__team_object__league__highest_parent__name__icontains=league)
+                for league in leagues
+            )
+            query = reduce(operator.or_, league)
+            queryset = queryset.filter(query)
 
         if self.filter_first_last is not None:
             queryset = queryset.annotate(fullname=Concat('first_name', Value(' '), 'last_name'))
@@ -194,8 +198,15 @@ class TeamsTable(TableView):
     page_title = 'Baza drużyn'
 
     def filter_queryset(self, queryset):
+
         if self.filter_league is not None:
-            queryset = queryset.filter(league__highest_parent__name__in=self.filter_league)
+            leagues = [league for league in self.filter_league]
+            league = (Q(
+                league__highest_parent__name__icontains=league)
+                for league in leagues
+            )
+            query = reduce(operator.or_, league)
+            queryset = queryset.filter(query)
 
         if self.filter_vivo is not None:
             vivos = [i.replace('-', '') for i in self.filter_vivo]
