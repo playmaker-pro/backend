6efb9efad318d385c2354cddfa3c6651bfff8a14
rkesik <rafal.kesik@gmail.com>
2022-05-24 12:40:46 +0200

    adding precalciulated full_name filed into clubs.Team for better performacnce

--- clubs/api/serizalizer.py
+++ clubs/api/serizalizer.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     text = serializers.SerializerMethodField()
 
-    def get_text(self, obj: Team):
-        return obj.get_pretty_name()
+    def get_text(self, obj):
+        return obj.full_name or obj.name
 
     class Meta:
         model = Team
