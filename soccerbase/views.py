b0154e053accc8bb3040e2eb45ca037e96542d96
janouinteria@gmail.com <janouinteria@gmail.com>
2022-07-04 22:46:32 +0200

    Removed icontains clause from coaches and players soccerbase filtering

--- soccerbase/views.py
+++ soccerbase/views.py
@@ -173,11 +173,11 @@ class PlayerTalbeQuickFilter(generic.TemplateView, mixins.PaginateMixin, mixins.
         if self.filter_vivo is not None:
             vivos = [i for i in self.filter_vivo]
 
-            clauses = (Q(playerprofile__voivodeship__icontains=p) for p in vivos)
+            clauses = (Q(playerprofile__voivodeship=p) for p in vivos)
             query = reduce(operator.or_, clauses)
 
             players_with_no_vivo = queryset.filter(playerprofile__voivodeship__isnull=True)
-            no_vivo_clause = (Q(playerprofile__team_object__club__voivodeship__name__icontains=p) for p in vivos)
+            no_vivo_clause = (Q(playerprofile__team_object__club__voivodeship__name=p) for p in vivos)
             no_vivo_query = reduce(operator.or_, no_vivo_clause)
 
             second_queryset = players_with_no_vivo.filter(no_vivo_query)
