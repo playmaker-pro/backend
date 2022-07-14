e5f8be7d37e42a9a756d15efde820a21a8133366
jakub.bartnik <jakub.bartnik@comarch.com>
2022-07-15 00:46:12 +0200

    team search tests update

--- clubs/tests/test_team_search.py
+++ clubs/tests/test_team_search.py
@@ -1,75 +1,65 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from clubs.models import Team, Club
+from clubs.models import Team
 from rest_framework.status import HTTP_200_OK
+from parameterized import parameterized
+from .test_club_search import ClubFactory
 
-class TeamSearchTest(APITestCase):
+class TeamFactory(ClubFactory):
 
-    url = reverse("resources:teams_search")
+    def __init__(self):
+        super().__init__()
 
-    def setUp(self):
         self.team1 = Team.objects.create(
             name="Barca",
-            club=Club.objects.create(
-               name="FC Barcelona",
-               country="ES"
-                )
+            club=self.club1
             )
         self.team2 = Team.objects.create(
             name="ManUtd",
-            club=Club.objects.create(
-               name="Manchester United F.C.",
-               country="EN"
-                )
+            club=self.club2
             )
         self.team3 = Team.objects.create(
             name="Bayern",
-            club=Club.objects.create(
-               name="FC Bayern Munchen",
-               country="DE"
-                )
+            club=self.club3
             )
         self.team4 = Team.objects.create(
             name="FC Bayern",
-            club=Club.objects.create(
-               name="FC Bayern",
-               country="DE"
-                )
+            club=self.club3
             )
         self.team5 = Team.objects.create(
             name="jakisfcKlub",
-            club=Club.objects.create(
-               name="Jakis Klub",
-               country="PL"
-                )
+            club=self.club1
             )
 
 
+class TeamSearchTest(APITestCase):
+
+    team_search_url = reverse("resources:teams_search")
+
+    def setUp(self):
+        self.teams = TeamFactory()        
+
     def test_response_200(self):
-        self.assertEqual(self.client.get(self.url).status_code, HTTP_200_OK)
+        self.assertEqual(self.client.get(self.team_search_url).status_code, HTTP_200_OK)
 
-
-    def test_name_contains(self):       
-        case1 = self.client.get(self.url, {"q": "Barca"})
-        case2 = self.client.get(self.url, {"q": "FC"})
-        case3 = self.client.get(self.url, {"q": ""})
-        case4 = self.client.get(self.url, {"q": "Bayern"})
-        case5 = self.client.get(self.url, {"q": "somerandomtext"})
-        case6 = self.client.get(self.url, {"q": "0"})
-        case7 = self.client.get(self.url, {"q": True})
-        case8 = self.client.get(self.url, {"q": "F" + "C"})
-        case9 = self.client.get(self.url, {"q": self.team4})
-
-        self.assertEqual(len(case1.data["results"]), 1)
-        self.assertEqual(len(case2.data["results"]), 2)
-        self.assertEqual(len(case3.data["results"]), 5)
-        self.assertEqual(len(case4.data["results"]), 2)
-        self.assertEqual(len(case5.data["results"]), 0)
-        self.assertEqual(len(case6.data["results"]), 0)
-        self.assertEqual(len(case7.data["results"]), 0)
-        self.assertEqual(len(case8.data["results"]), 2)
-        self.assertEqual(len(case9.data["results"]), 0)
-
+    @parameterized.expand([
+        ("Barca", 1),
+        ("FC", 2),
+        ("", 5),
+        ("Bayern", 2),
+        ("randomtext", 0),
+        (0, 0),
+        (True, 0),
+        ("a", 5),
+        ("Team.objects.all()", 0),
+        ("Team.objects.get(id=1)", 0)
+    ])
+    def test_team_name_contains(self, query_param, response_obj_count):
+        self.assertEqual(
+            len(self.client.get(
+                self.team_search_url, {"q": query_param}).data["results"]),
+                response_obj_count
+            )
 
     def test_objects_created_successfully(self):
         self.assertEqual(Team.objects.all().count(), 5)
