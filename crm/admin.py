93370909af8a6b61f3ce3760efac0b48846a631b
jakub.bartnik <jakub.bartnik@comarch.com>
2022-07-27 02:59:38 +0200

    admin site adjustment, model save final

--- crm/admin.py
+++ crm/admin.py
@@ -36,22 +36,37 @@ class LeadStatusAdmin(admin.ModelAdmin):
     
     list_display = (
         "id",
-        "first_name",
-        "last_name",
+        "full_name",
         linkify("user"),
-        linkify("club"),
+        "phone",
+        "email",
+        "get_user_role",
         linkify("team"),
-        "date_created",
-        "date_updated",
-        linkify("created_by"),
-        linkify("updated_by"),
+        linkify("club"),
         "is_actual",
+        "data_mapper_id",
+        "twitter_url",
+        "facebook_url",
+        "linkedin_url",
+        "instagram_url",
+        "website_url",
+        linkify("created_by"),
+        "date_created",
+        linkify("updated_by"),
+        "date_updated",        
         linkify("previous"),
         linkify("next"),
     )
 
+    list_display_links = ("id", "full_name")
+
     list_filter = ["is_actual"]
 
+    def get_user_role(self, obj):
+        return obj.user.get_declared_role_display() if obj.user else None
+
+    get_user_role.short_description = "User Role"
+    
     def save_model(self, request, obj, form, change):
         if obj.id == None:
             obj.created_by = request.user
