0ece8099f48fef5ae6b941c025bbfc9caeadd311
janou <l.remkowicz@gmail.com>
2022-07-28 20:59:58 +0200

    Added fixed btn to main div.

--- backend/static/assets/js/landingpage/observe_btn.js
+++ backend/static/assets/js/landingpage/observe_btn.js
@@ -1,28 +1,33 @@
 const observeBtn = document.querySelector('.observe-div');
 const disabledBtn = document.querySelector('.main__btn');
+let testReadyBtn = document.querySelector('.ready_for_tests_unauthenticated a');
+
+const btnAuthenticated = document.querySelector('#test__section .user_is_authenticated')
+
+testReadyBtn = testReadyBtn ?? btnAuthenticated
 
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
+        rect.top >= -100 &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
-function changeVisibility(){
-    disabledBtn.classList.remove('d-flex');
-    disabledBtn.classList.add('d-none');
-}
+document.onscroll = function() {
 
-document.addEventListener('scroll', (event) => {
-    console.log(isInViewport(observeBtn) && disabledBtn.classList.contains('d-flex') && disabledBtn.style.getPropertyValue('opacity') === '0')
-    if (!isInViewport(observeBtn) && disabledBtn.classList.contains('d-none')){
-        disabledBtn.classList.remove('d-none');
-        disabledBtn.classList.add('d-flex');
-        disabledBtn.style.zIndex = 100;
-        disabledBtn.style.opacity = 1;
+    const hiddenElementRangeStart = window.scrollY + disabledBtn.getBoundingClientRect().top;
+    const TestReadyTop = window.scrollY + testReadyBtn.getBoundingClientRect().top;
+
+    if(hiddenElementRangeStart >= TestReadyTop || window.scrollY < 590){
+        disabledBtn.classList.remove('main__btn__toggle');
+        testReadyBtn.style.visibility = "visible"
+    }
+    else if (window.scrollY >= 590 && !disabledBtn.classList.contains('main__btn__toggle') && window.scrollY <= 1000){
+        testReadyBtn.style.visibility = "hidden"
+        disabledBtn.classList.add('main__btn__toggle');
         disabledBtn.animate([
             {opacity:0},
             {opacity:0.25},
@@ -30,21 +35,57 @@ document.addEventListener('scroll', (event) => {
             {opacity:0.75},
             {opacity:1},
         ],
-             {duration:2000, fill: 'forwards'}
+             {duration:800}
         )
-    } else if (isInViewport(observeBtn) && disabledBtn.classList.contains('d-flex') && disabledBtn.style.getPropertyValue('opacity') === '1'){
-        console.log('odpalam')
-        const animate = disabledBtn.animate([
-            {opacity:1},
-            {opacity:0.75},
-            {opacity:0.5},
+    }
+    else{
+        testReadyBtn.style.visibility = "hidden"
+        disabledBtn.classList.add('main__btn__toggle');
+    }
+
+};
+
+// the callback function to be provided to the IntersectionObserver;
+// entries: an Array of IntersectionObserverEntries,
+// o: a reference to the options Object passed to the IntersectionObserver:
+function toggle(entries, o) {
+
+  // finding the relevant element:
+    console.log('observer')
+    console.log('!entries[0].isIntersecting', !entries[0].isIntersecting)
+
+    if (!entries[0].isIntersecting){
+        disabledBtn.classList.add('main__btn__toggle');
+        disabledBtn.animate([
+            {opacity:0},
             {opacity:0.25},
-            {opacity:0, display: 'none'},
+            {opacity:0.5},
+            {opacity:0.75},
+            {opacity:1},
         ],
-             {duration:300}
+             {duration:800}
         )
-        const myTimeout = setTimeout(changeVisibility, 400);
+    } else {
+        disabledBtn.classList.remove('main__btn__toggle');
 
     }
-});
 
+  // disabledBtn
+  //
+  //   // updating its class-name, adding the 'inViewport' class
+  //   // if entries[0].isIntersecting is true (the observed
+  //   // element is in the viewport) or removing the class if
+  //   // entries[0].isIntersecting is false (the observed element
+  //   // is not in the viewport):
+  //   .classList.toggle('main__btn__toggle', entries[0].isIntersecting);
+
+}
+
+// initialising the IntersectionObserver, and assigning the
+// handler function:
+// let observer = new IntersectionObserver(toggle);
+//
+// // specifying which element the IntersectionObserver should
+// // observe:
+// observer.observe(observeBtn);
+//
