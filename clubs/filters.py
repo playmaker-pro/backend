b908bdc9973db5b093ac5d5cf8f444302157148c
k.garczewski <garczewskikrzysztof@gmail.com>
2023-01-10 10:51:00 +0100

    added docstrings

--- clubs/filters.py
+++ clubs/filters.py
@@ -2,17 +2,28 @@ from django.contrib.admin import SimpleListFilter
 
 
 class IsParentFilter(SimpleListFilter):
+    """
+    A Django SimpleListFilter subclass that allows the user to filter the queryset
+    based on whether the object is the highest parent or not.
+    """
     title = "is highest parent"
     parameter_name = "isparent"
 
     def lookups(self, request, model_admin):
-
+        """
+        Returns a list of tuples with the values to filter by
+        and the display names for the filter. True, False for
+        the option will appear in the right sidebar
+        """
         return [
             ("true", "True"),
             ("false", "False"),
         ]
 
     def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the selected value in the filter.
+        """
         if self.value() == "true":
             return queryset.distinct().filter(parent__isnull=True)
         if self.value() == 'false':
@@ -20,14 +31,26 @@ class IsParentFilter(SimpleListFilter):
 
 
 class CountryListFilter(SimpleListFilter):
+    """
+    A Django SimpleListFilter subclass that allows the user
+    to filter the queryset based on the country field.
+    """
     title = 'Country'
     parameter_name = 'country'
 
     def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples with the values to filter by
+        and the display names for the filter. Country full name for
+        the option will appear in the right sidebar.
+        """
         countries = set([c.country for c in model_admin.model.objects.all()])
         return [(country, country.name) for country in countries]
 
     def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the selected value in the filter.
+        """
         if self.value():
             return queryset.filter(country=self.value())
         else:
@@ -35,10 +58,19 @@ class CountryListFilter(SimpleListFilter):
 
 
 class HasManagerFilter(SimpleListFilter):
+    """
+    A Django SimpleListFilter subclass that allows the user
+    to filter the queryset based on whether the object has a manager or not.
+    """
     title = "hasManager"
     parameter_name = "manager"
 
     def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples with the values to filter
+        by and the display names for the filter. True, False for
+        the option will appear in the right sidebar
+        """
 
         return [
             ("true", "True"),
@@ -46,6 +78,9 @@ class HasManagerFilter(SimpleListFilter):
         ]
 
     def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the selected value in the filter.
+        """
         if self.value() == "true":
             return queryset.distinct().filter(manager__isnull=False)
         if self.value():
