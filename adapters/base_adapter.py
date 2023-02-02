d771d925bdc72dc52398a1f1c3c0570e53b8523e
bartnyk <kuba.bartnik114@gmail.com>
2023-02-02 23:31:35 +0000

    PM-570 player adapter

---
+++ adapters/base_adapter.py
@@ -0,0 +1,27 @@
+import typing
+
+from pm_core.services.base import PMServiceBase
+from adapters.strategy import Strategy
+from pm_core.services.scrapper_service import ScrapperAPI
+
+
+class BaseAdapter:
+    def __init__(
+        self,
+        strategy: typing.Type[Strategy],
+        api_method: typing.Type[PMServiceBase] = ScrapperAPI,
+        meta: typing.Dict = None,
+    ) -> None:
+        self.strategy: Strategy = strategy()
+        self.meta: typing.Dict = meta
+        self.api: typing.Union[PMServiceBase] = api_method()
+
+    def resolve_strategy(self) -> typing.Dict:
+        """
+        Configure params to fetch data based on defined strategy
+        """
+        return {
+            attr.lower(): getattr(self.strategy, attr)
+            for attr in dir(self.strategy)
+            if not callable(getattr(self.strategy, attr)) and not attr.startswith("_")
+        }
