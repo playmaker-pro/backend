17668311a2df7b862e393c01591eb94a6e491bbb
k.garczewski <garczewskikrzysztof@gmail.com>
2023-03-20 01:07:36 +0100

    Add functionality to delete ExternalLinksEntity when URL is empty.Update comments and docstrings in external links module

--- external_links/utils.py
+++ external_links/utils.py
@@ -16,21 +16,22 @@ LINK_TYPES = {
     'other': 'social',
 }
 
-
-def create_or_update_player_external_links(obj: Union[
+EXT_LINK_MODEL = Union[
     'PlayerProfile', 'CoachProfile', 'ScoutProfile', 'ManagerProfile', 'Club', 'Team', 'LeagueHistory'
-]) -> None:
-    """
-    Update or create ExternalLinks instances for a given profile object, which can be any of the following types:
-    'PlayerProfile', 'CoachProfile', 'ScoutProfile', 'ManagerProfile', 'Club', 'Team', or 'League'. The function
-    first updates or creates the ExternalLinks instance that represents the profile's external links. If the instance is
-    created, it is associated with the profile object. It then retrieves all LinkSource objects from the database and
-    collects all links from the given profile object by iterating through its fields and adding those that end in "_url"
-    and have a non-empty value. For each link, it creates or updates an ExternalLinksEntity instance with the
-    corresponding LinkSource and adds it to the ExternalLinks instance.
-    """
+]
 
-    "Update or create the ExternalLinks instance for the profile"
+
+def create_or_update_player_external_links(obj: EXT_LINK_MODEL) -> None:
+
+    # Update or create ExternalLinks instances for a given profile object, which can be any of the following types:
+    # 'PlayerProfile', 'CoachProfile', 'ScoutProfile', 'ManagerProfile', 'Club', 'Team', or 'League'. The function
+    # first updates or creates the ExternalLinks instance that represents the profile's external links. If the instance
+    # is created, it is associated with the profile object. It then retrieves all LinkSource objects from the database
+    # and collects all links from the given profile object by iterating through its fields and adding those that
+    # end in "_url" and have a non-empty value. For each link, it creates or updates an ExternalLinksEntity instance
+    # with the corresponding LinkSource and adds it to the ExternalLinks instance.
+
+    # "Update or create the ExternalLinks instance for the profile"
     model_name = type(obj).__name__.lower()
     external_links, created = ExternalLinks.objects.update_or_create(
         **{f"{model_name}": obj}
@@ -39,24 +40,32 @@ def create_or_update_player_external_links(obj: Union[
         setattr(obj, 'external_links', external_links)
         obj.save()
 
-    "Get all link sources from the database"
+    # "Get all link sources from the database"
     link_sources = LinkSource.objects.all()
 
-    "Get all link fields from the profile model"
+    # "Get all link fields from the profile model"
     link_fields = [field.name for field in obj._meta.get_fields() if field.name.endswith("_url")]
 
-    "Collect all links for the profile"
+    # "Determine the related type for the ExternalLinksEntity instance based on the profile model name"
+    related_type = model_name.replace("profile", "")
+
+    # "Collect all links for the profile"
     all_links = []
     for link_field in link_fields:
         link = getattr(obj, link_field, None)
-        if link:
-            source_name = link_field[:-4]  # Remove "_url" suffix
-            link_source = link_sources.filter(name=source_name).first()
-            if link_source:
-                all_links.append((link, link_source))
-
-    "Determine the related type for the ExternalLinksEntity instance based on the profile model name"
-    related_type = model_name.replace("profile", "")
+        source_name = link_field[:-4]  # Remove "_url" suffix
+        link_source = link_sources.filter(name=source_name).first()
+        if link and link_source:
+            all_links.append((link, link_source))
+        # Delete ExternalLinksEntity with empty URL when user removes URL from profile
+        elif link_source:
+            entity = ExternalLinksEntity.objects.filter(
+                target=external_links,
+                source=link_source,
+                related_type=related_type,
+            ).first()
+            if entity:
+                entity.delete()
 
     for link, link_source in all_links:
         entity, created = ExternalLinksEntity.objects.get_or_create(
