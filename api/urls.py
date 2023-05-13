16a07d0bd5a3cd22d78eaa52643a5e09c6aa961e
bartnyk <kuba.bartnik114@gmail.com>
2023-05-13 15:37:49 +0200

    tests fix

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import include, url
 from django.urls import path
-
 from users.api_urls import urlpatterns as users_urls
 from clubs.api.api_urls import urlpatterns as clubs_urls
 from profiles.api_urls import urlpatterns as profiles_urls
-
 from rest_framework.authtoken import views
 
+app_name = "api"
 
 urlpatterns = [
     url(r"^users/", include((users_urls, "users"))),
