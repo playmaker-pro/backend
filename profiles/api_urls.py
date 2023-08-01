895d8a40f63c6462a2116c62673b6fe8cc086bc2
bartnyk <kuba.bartnik114@gmail.com>
2023-08-02 00:38:45 +0200

    PM20-245: removed playerupdate route

--- profiles/api_urls.py
+++ profiles/api_urls.py
@@ -1,26 +1,27 @@
 from django.urls import path
 from rest_framework import routers
-from .apis import ProfileAPI, FormationChoicesView
-from resources.views import WebhookPlayer
+from . import apis as views
+
+# from resources.views import WebhookPlayer
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register("", ProfileAPI, basename="profiles")
+router.register("", views.ProfileAPI, basename="profiles")
 
 urlpatterns = [
     path(
         r"",
-        ProfileAPI.as_view({"post": "create_profile", "patch": "update_profile"}),
+        views.ProfileAPI.as_view({"post": "create_profile", "patch": "update_profile"}),
         name="create_or_update_profile",
     ),
     path(
         r"<uuid:profile_uuid>/",
-        ProfileAPI.as_view({"get": "get_profile_by_uuid"}),
+        views.ProfileAPI.as_view({"get": "get_profile_by_uuid"}),
         name="get_profile",
     ),
-    path(r"playerupdate/", WebhookPlayer.as_view(), name="player_webhook"),
+    # path(r"playerupdate/", WebhookPlayer.as_view(), name="player_webhook"), DEPRECATED: PM20-245
     path(
         r"formations/",
-        FormationChoicesView.as_view({"get": "list_formations"}),
+        views.FormationChoicesView.as_view({"get": "list_formations"}),
         name="formations_list",
     ),
 ]
