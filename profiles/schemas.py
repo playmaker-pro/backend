3c67ead5b48d2a1f0cc81c563c34d5f85c077c9f
Lukas Remkowicz <l.remkowicz@gmail.com>
2024-01-02 20:12:18 +0000

    Fixes for transfer request + changes in player positions model.

--- profiles/schemas.py
+++ profiles/schemas.py
@@ -88,7 +88,7 @@ class TransferRequestSchema(BaseModel):
     requesting_team: dict
     gender: str
     status: dict
-    position: dict
+    player_position: dict
     number_of_trainings: dict
     benefits: List[dict]
     salary: dict
