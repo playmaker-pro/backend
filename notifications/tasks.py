c977e381e84469c68e360c74acb5a0ea0e27afc7
bartnyk <kuba.bartnik114@gmail.com>
2025-02-11 13:52:03 +0100

    revert

--- notifications/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-
-from notifications.models import Notification
-
-
-@shared_task
-def create_notification(*args, **kwargs) -> Notification:
-    return Notification.objects.create(*args, **kwargs)
