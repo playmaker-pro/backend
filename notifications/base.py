05ec185aa3ff59cd6a5f6efcada084231ed52171
bartnyk <kuba.bartnik114@gmail.com>
2025-04-15 18:41:35 +0200

    Remove deprecated notification management commands and related services
    - Deleted `not_initial.py`, `send.py`, `send_profile_completion_notifications.py`, and `mass.py` management commands.
    - Removed `message.py`, `services.py`, `signals.py`, and utility functions related to notifications.
    - Cleared out associated test files for removed functionalities, including tests for model creation, notification service, API, and template rendering.
    - This cleanup aims to streamline the notification system by eliminating unused code and improving maintainability.

--- notifications/base.py
+++
@@ -1,2 +0,0 @@
-class PMMailBase:
-    pass
--- notifications/html.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import send_mail
-
-subject = "That’s your subject"
-html_message = render_to_string("mail_template.html", {"context": "values"})
-plain_message = strip_tags(html_message)
-from_email = "from@yourdjangoapp.com>"
-to = "to@yourbestuser.com"
-
-mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
