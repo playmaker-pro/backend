b4c12ae09008560c4459b46117c61f7cd5f3b94e
Jakub Bartnik <j.bartnik@contina.pl>
2025-08-21 13:10:22 +0200

    feat: add custom email templates directory to template settings

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -134,7 +134,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, ".custom_email_templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
