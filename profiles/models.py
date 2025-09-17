d3721410a0277e9f925c132134cddd679c5c09f4
Jakub Bartnik <jakub@playmaker.pro>
2025-09-17 15:47:51 +0200

    fix: Update __str__ method in ProfileMeta to include profile class and user full name

--- profiles/models.py
+++ profiles/models.py
@@ -2415,7 +2415,7 @@ class ProfileMeta(models.Model, VisitationMixin):
         return transfer_obj is not None and transfer_obj.is_anonymous
 
     def __str__(self) -> None:
-        return f"Meta of {self.profile}"
+        return f"Meta of [{self._profile_class}] {self.user.get_full_name()}"
 
     class Meta:
         verbose_name = "Meta"
